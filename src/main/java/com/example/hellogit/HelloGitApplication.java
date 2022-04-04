b2800b1e055ecd22d7bb81c26b481b0b4337e337
Chihoon Lee <clgns1202@gmail.com>
2022-04-04 23:40:19 +0900

    STEP by STEP

---
+++ src/main/java/com/example/hellogit/HelloGitApplication.java
@@ -0,0 +1,15 @@
+package com.example.hellogit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloGitApplication {
+
+	public static void main(String[] args) {
+//		SpringApplication.run(HelloGitApplication.class, args);
+
+		System.out.println("STEP BY STEP");
+	}
+
+}
