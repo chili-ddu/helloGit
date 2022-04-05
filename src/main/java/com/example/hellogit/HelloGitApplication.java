e67a94c06db01c59eb0689a39f897877d86ddd9d
Chihoon Lee <clgns1202@gmail.com>
2022-04-05 22:52:57 +0900

    day 2

--- src/main/java/com/example/hellogit/HelloGitApplication.java
+++ src/main/java/com/example/hellogit/HelloGitApplication.java
@@ -1,15 +1,19 @@
 package com.example.hellogit;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Slf4j
 public class HelloGitApplication {
+
 
 	public static void main(String[] args) {
 //		SpringApplication.run(HelloGitApplication.class, args);
 
-		System.out.println("STEP BY STEP");
+		log.info("Hello?");
+
 	}
 
 }
